2c5e697ef1bbe25f0cbb5c1f708b813c7dadf63c
lavochka <name.space.std@outlook.com>
2026-08-29 05:06:19 +0700

    add: login_frame, waiting_frame, builds. changed: new commands

--- game_controller.py
+++ game_controller.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-from game_logic import GameLogic
+from models import User, Player
+from room import Room
 
 # -- Constants --
 LABEL_SIZE = 20
@@ -20,7 +21,9 @@ class GameController:
         self.root = root
 
         # -- Frames --
+        self.login_frame = ctk.CTkFrame(root)
         self.menu_frame = ctk.CTkFrame(root)
+        self.waiting_frame = ctk.CTkFrame(root)
         self.game_frame = ctk.CTkFrame(root)
         self.finish_frame = ctk.CTkFrame(root)
 
@@ -32,9 +35,11 @@ class GameController:
         self.animation_after_ids = []
 
         # -- Atributs --
+        self._build_login_ui()
         self._build_menu_ui()
+        self._build_waiting_ui()
         self._build_finish_ui()
-        self._show_menu()
+        self._show_login()
 
     # Start game
     def start_game(self):
@@ -45,7 +50,13 @@ class GameController:
         size = int(self.selected_size.split('x')[0])
 
         # Start logic
-        self.game_logic = GameLogic(size)
+        self.players = [
+            Player(User(self.username)),
+            Player(User("User2"))
+        ]
+
+        self.room = Room(size, self.players)
+        self.game_logic = self.room.game_logic
         self.game_logic.generate_cards()
 
         # Start funcions
@@ -53,9 +64,15 @@ class GameController:
         self._update_timer()
         self._show_game()
 
+    def _show_login(self):
+        self._switch_frame(self.login_frame)
+
     # Close all and open menu
     def _show_menu(self):
         self._switch_frame(self.menu_frame)
+
+    def _show_waiting(self):
+        self._switch_frame(self.waiting_frame)
 
     # Close all and open gmae
     def _show_game(self):
@@ -64,6 +81,21 @@ class GameController:
     # Close all and open finish
     def _show_finish(self):
         self._switch_frame(self.finish_frame)
+
+    def _build_login_ui(self):
+        self.login_frame.grid_columnconfigure(0, weight=1)
+
+        # -- Labels --
+        login_main_lbl = ctk.CTkLabel(self.login_frame, text="Enter username")
+        login_main_lbl.grid(row=0, column=0, padx=LABEL_SIZE, pady=LABEL_SIZE, sticky="ew")
+
+        # -- Input --
+        self.login_input_etr = ctk.CTkEntry(self.login_frame, placeholder_text="username")
+        self.login_input_etr.grid(row=1, column=0)
+
+        # -- Buttons --
+        login_continue_btn = ctk.CTkButton(self.login_frame, text="Continue", command=self.login)
+        login_continue_btn.grid(row=3, column=0, padx=MENU_BUTTON_SIZE, pady=MENU_BUTTON_SIZE, sticky="ew")
 
     # Create main menu
     def _build_menu_ui(self):
@@ -83,11 +115,22 @@ class GameController:
         menu_range_seg.grid(row=1, column=0, padx=SEGMENT_SIZE, pady=SEGMENT_SIZE, sticky="ew")
 
         # -- Buttons --
-        menu_start_btn = ctk.CTkButton(self.menu_frame, text="Start", command=self.start_game)
+        menu_start_btn = ctk.CTkButton(self.menu_frame, text="Start", command=self.join_room)
         menu_start_btn.grid(row=2, column=0, padx=MENU_BUTTON_SIZE, pady=MENU_BUTTON_SIZE, sticky="ew")
+
+    def _build_waiting_ui(self):
+        self.waiting_frame.grid_columnconfigure(0, weight=1)
+
+        # -- Labels --
+        waiting_main_lbl = ctk.CTkLabel(self.waiting_frame, text="Waiting...")
+        waiting_main_lbl.grid(row=0, column=0, padx=LABEL_SIZE, pady=LABEL_SIZE, sticky="ew")
 
     # Build only widgets
     def _build_game_ui(self, size):
+        # -- Players --
+        player1 = self.game_logic.players[0]
+        player2 = self.game_logic.players[1]
+        
         # Clear all
         for widget in self.game_frame.winfo_children():
             widget.destroy()
@@ -125,15 +168,15 @@ class GameController:
         self.game_timer_lbl.grid(row=size, column=0, columnspan=size, pady=(10, 0))
 
         # Turn label
-        self.game_current_turn_lbl = ctk.CTkLabel(self.board_frame, text=f"Turn: User1")
+        self.game_current_turn_lbl = ctk.CTkLabel(self.board_frame, text=f"Turn: {player1.user.username}")
         self.game_current_turn_lbl.grid(row=size + 1, column=0, columnspan=size, pady=(10, 0))
 
         # Users status
-        self.game_user1_lbl = ctk.CTkLabel(self.right_user_frame, text="User1\n- Score: 0\n- Streak: 0\n- Clicks: 0")
-        self.game_user1_lbl.pack(padx=15, pady=15)
+        self.game_player1_lbl = ctk.CTkLabel(self.left_user_frame, text=f"{player1.user.username}\n- Score: 0\n- Streak: 0\n- Clicks: 0")
+        self.game_player1_lbl.pack(padx=15, pady=15)
         
-        self.game_user2_lbl = ctk.CTkLabel(self.left_user_frame, text="User2\n- Score: 0\n- Streak: 0\n- Clicks: 0")
-        self.game_user2_lbl.pack(padx=15, pady=15)
+        self.game_player2_lbl = ctk.CTkLabel(self.right_user_frame, text=f"{player2.user.username}\n- Score: 0\n- Streak: 0\n- Clicks: 0")
+        self.game_player2_lbl.pack(padx=15, pady=15)
 
         # Back button
         game_back_btn = ctk.CTkButton(self.board_frame, text="Back", command=self.back_to_menu)
@@ -164,7 +207,7 @@ class GameController:
         self._animate_flip(idx, card.value)
 
         # Clicks update
-        self._update_users_info()
+        self._update_players_info()
 
         # Check first_card == second_cars
         if len(self.game_logic.opened) == 2:
@@ -204,7 +247,7 @@ class GameController:
     def _resolve_check(self):
         # Get return in check_match
         result = self.game_logic.check_match()
-        self._update_users_info()
+        self._update_players_info()
         # Get first and second
         for card in(result["first_card"], result["second_card"]):
             idx = self._card_index(card.row, card.col)
@@ -254,7 +297,21 @@ class GameController:
         self.game_logic.stop_timer()
 
         # Check winner
-        finish_text = self.game_logic.get_winner()
+        winner = self.game_logic.get_winner()
+        minutes, seconds = divmod(self.game_logic.seconds_passed, TIMER_MIN)
+
+        if winner is None:
+            finish_text = (
+                "DRAW!\n\n"
+                f"Time: {minutes}:{seconds:02d}"
+            )
+        else:
+            finish_text = (
+                f"Winner: {winner.user.username}\n\n"
+                f"Clicks: {winner.clicks}\n"
+                f"Score: {winner.score}\n"
+                f"Time: {minutes}:{seconds:02d}"
+            )
 
         # Cange finish label
         self.finish_main_lbl.configure(text=finish_text)
@@ -277,7 +334,16 @@ class GameController:
 
     # Hide all frames, show only the given one
     def _switch_frame(self, frame):
-        for f in (self.menu_frame, self.game_frame, self.finish_frame):
+        # All frames
+        frames = [
+            self.login_frame,
+            self.menu_frame,
+            self.waiting_frame,
+            self.game_frame,
+            self.finish_frame
+        ]
+
+        for f in frames:
             f.pack_forget()
         if frame == self.game_frame:
             frame.pack(fill="both", expand=True)
@@ -292,21 +358,31 @@ class GameController:
     def _card_index(self, row, col):
         return row * self.game_logic.size + col
 
-    def _update_users_info(self):
-        # -- Users --
-        user1 = self.game_logic.users[0]
-        user2 = self.game_logic.users[1]
-        current_user = self.game_logic.current_user()
+    def _update_players_info(self):
+        # -- Players --
+        player1 = self.game_logic.players[0]
+        player2 = self.game_logic.players[1]
+        current_player = self.game_logic.current_player()
 
-        self.game_current_turn_lbl.configure(text=f"Turn: {current_user.name}")
+        self.game_current_turn_lbl.configure(text=f"Turn: {current_player.user.username}")
 
-        self.game_user1_lbl.configure(
-            text=f"{user1.name}\n- Score: {user1.score}\n- Streak: {user1.streak}\n- Clicks: {user1.clicks}"
+        self.game_player1_lbl.configure(
+            text=f"{player1.user.username}\n- Score: {player1.score}\n- Streak: {player1.streak}\n- Clicks: {player1.clicks}"
         )
 
-        self.game_user2_lbl.configure(
-            text=f"{user2.name}\n- Score: {user2.score}\n- Streak: {user2.streak}\n- Clicks: {user2.clicks}"
+        self.game_player2_lbl.configure(
+            text=f"{player2.user.username}\n- Score: {player2.score}\n- Streak: {player2.streak}\n- Clicks: {player2.clicks}"
         )
 
+    def join_room(self):
+        self._show_waiting()
+        # TODO - server GET
+        self.root.after(TIMER_DELAY * 3, self.start_game)
 
-        
+    def login(self):
+        self.username = self.login_input_etr.get()
+
+        if self.username == "":
+            self.username = "User1"
+
+        self._show_menu()
