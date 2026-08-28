00ab2e2ccfc38679f710c260c2b4ff3da8808d69
lavochka <name.space.std@outlook.com>
2026-08-29 05:24:11 +0700

    ref

--- room.py
+++
@@ -1,8 +0,0 @@
-from game_logic import GameLogic
-
-class Room:
-    def __init__(self, size, players):
-        self.players = players
-        self.status = "waiting"
-
-        self.game_logic = GameLogic(size, self.players)
