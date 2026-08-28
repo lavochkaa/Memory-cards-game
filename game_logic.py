24a98777ccd9dbf8c226b758ba93c6a493845dfe
lavochka <name.space.std@outlook.com>
2026-08-29 02:16:07 +0700

    add: logic to 2 users, replace funcs from game_controller, new __init__

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,6 @@
 import random
 from models import Card
+from models import User
 
 # -- Constants --
 EMOJIS = [
@@ -9,6 +10,7 @@ EMOJIS = [
     "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "🦄"
 ]
 POINTS_MATCH = 10
+TIMER_MIN = 60
 
 # -- Game logic --
 class GameLogic:
@@ -17,16 +19,20 @@ class GameLogic:
         # Get size
         self.size = size
 
+        # -- Users --
+        self.users = [
+            User("User1"),
+            User("User2")
+        ]
+
         # -- Masives --
         self.cards = []
         self.opened = []
 
         # -- Basic values --
-        self.clicks = 0
         self.matched_count = 0
         self.seconds_passed = 0
-        self.score = 0
-        self.streak = 0
+        self.current_user_id = 0
 
         # -- Flags --
         self.timer_running = False
@@ -68,7 +74,7 @@ class GameLogic:
         self.opened.append(card)
 
         # Save click
-        self.clicks += 1
+        self.current_user().clicks += 1
 
         return True
 
@@ -78,20 +84,23 @@ class GameLogic:
         first_card, second_card = self.opened
         is_match = first_card.value == second_card.value
 
+        # Save user
+        user = self.current_user()
+
         # if first_card == second_card -> cool
-        graind = 0
         if is_match:
             first_card.is_matched = True
             second_card.is_matched = True
             self.matched_count += 2
 
-            self.streak += 1
-            graind = POINTS_MATCH * self.streak
-            self.score += graind
+            user.streak += 1
+            graind = POINTS_MATCH * user.streak
+            user.score += graind
         else:
             first_card.is_open = False
             second_card.is_open = False
-            self.streak = 0
+            user.streak = 0
+            self.switch_turn()
 
         # Clear opened cards and close
         self.opened.clear()
@@ -102,8 +111,6 @@ class GameLogic:
             "first_card": first_card,
             "second_card": second_card,
             "game_won": self.matched_count == len(self.cards),
-            "score": self.score,
-            "streak": self.streak,
         }
 
     # Advance the clock by one second
@@ -113,3 +120,43 @@ class GameLogic:
     # Stop the clock
     def stop_timer(self):
         self.timer_running = False
+
+    def current_user(self):
+        return self.users[self.current_user_id]
+
+    def switch_turn(self):
+        self.current_user_id = 1 - self.current_user_id
+
+    def get_winner(self):
+        # Get metrics
+        user1 = self.users[0]
+        user2 = self.users[1]
+        
+        # Get min and sec to change in finish
+        minutes, seconds = divmod(self.seconds_passed, TIMER_MIN)
+
+        if user1.score == user2.score:
+            finish_text = (
+                f"DRAW!\n\n"
+                f"Time: {minutes}:{seconds:02d}\n\n"
+                f"{user1.name} - Clicks: {user1.clicks} - Score: {user1.score}\n"
+                f"{user2.name} - Clicks: {user2.clicks} - Score: {user2.score}"
+            )
+        elif user1.score > user2.score:
+            winner = user1
+            finish_text = (
+                f"Winner: {winner.name}\n\n"
+                f"Time: {minutes}:{seconds:02d}\n"
+                f"Clicks: {winner.clicks}\n"
+                f"Score: {winner.score}"
+            )
+        else:
+            winner = user2
+            finish_text = (
+                f"Winner: {winner.name}\n\n"
+                f"Time: {minutes}:{seconds:02d}\n"
+                f"Clicks: {winner.clicks}\n"
+                f"Score: {winner.score}"
+            )
+            
+        return finish_text
